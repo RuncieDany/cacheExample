6bd04286e96d64078e8b45eaf9eb50c205345e54
Runcie <runciedany@gmail.com>
2018-12-20 16:56:20 -0500

    change@4:56PM

--- src/test/java/com/n26/controller/TransactionControllerTest.java
+++ src/test/java/com/n26/controller/TransactionControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -17,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.n26.model.Statistics;
@@ -37,10 +37,11 @@ public class TransactionControllerTest {
 	@Autowired
 	private TransactionService transactionService;
 	
-
+    @Autowired
 	private Statistics statistics;
+    @Autowired
 	private Transaction transaction;
-	
+	@Autowired
 	private ObjectMapper objectMapper;
 
 	@Before
@@ -55,13 +56,124 @@ public class TransactionControllerTest {
 	public void postTransaction_whileInvalidData_Failure() throws Exception {
 		
 
-		String jsonInput= "{ Example :123}"; 	
+		transaction.setAmount(new BigDecimal("45.46"));
+		//Instant instant= new Instant ();
+		Instant test=Instant.now();
+		transaction.setTimestamp(test);
 		MvcResult mvcResult = mockMVC.perform(post("/transaction")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content(jsonInput))
+				.content(objectMapper.writeValueAsString(transaction)))
 				.andExpect(status().isBadRequest()).andReturn();	
 		
+		System.out.println(mvcResult.getResponse().getCharacterEncoding());		
+		
+
+	}
+	
+	
+	
+	@Test
+	public void postTransaction_WhileValidData_Success() throws Exception {
+		
+
+		transaction.setAmount(new BigDecimal("45.46"));
+		Instant test=Instant.now();
+		transaction.setTimestamp(test);
+		MvcResult mvcResult = mockMVC.perform(post("/transaction")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(transaction)))
+				.andExpect(status().isCreated()).andReturn();	
+		
 		System.out.println(mvcResult.getResponse().getContentAsString());		
+		
+
+	}
+	
+	
+	
+	@Test
+	public void getStatistics_WhileValidData_Success() throws Exception {
+		
+
+		transaction.setAmount(new BigDecimal("45.46"));
+		Instant test=Instant.now();
+		
+		transaction.setTimestamp(test);
+		MvcResult mvcResult = mockMVC.perform(post("/transaction")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(transaction)))
+				.andExpect(status().isCreated()).andReturn();	
+		
+		transaction.setAmount(new BigDecimal("50.46"));
+				transaction.setTimestamp(test);
+		 mvcResult = mockMVC.perform(post("/transaction")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(transaction)))
+				.andExpect(status().isCreated()).andReturn();
+		 
+		 transaction.setAmount(new BigDecimal("13.46"));
+			transaction.setTimestamp(test);
+	 mvcResult = mockMVC.perform(post("/transaction")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(transaction)))
+			.andExpect(status().isCreated()).andReturn();	
+	
+		
+		
+		mvcResult = mockMVC.perform(get("/statistics"))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		
+		System.out.println(mvcResult.getResponse().getContentAsString());
+		
+
+	}
+	
+	
+
+	@Test
+	public void delete_WhileValidData_Success() throws Exception {
+		
+
+		transaction.setAmount(new BigDecimal("45.46"));
+		Instant test=Instant.now();
+		transaction.setTimestamp(test);
+		MvcResult mvcResult = mockMVC.perform(post("/transaction")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(transaction)))
+				.andExpect(status().isCreated()).andReturn();	
+		
+		transaction.setAmount(new BigDecimal("50.46"));
+				transaction.setTimestamp(test);
+		 mvcResult = mockMVC.perform(post("/transaction")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(transaction)))
+				.andExpect(status().isCreated()).andReturn();
+		 
+		 transaction.setAmount(new BigDecimal("13.46"));
+			transaction.setTimestamp(test);
+	 mvcResult = mockMVC.perform(post("/transaction")
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(transaction)))
+			.andExpect(status().isCreated()).andReturn();	
+	
+		
+		
+		mvcResult = mockMVC.perform(get("/statistics"))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		mvcResult = mockMVC.perform(delete("/transactions"))
+				.andExpect(status().isNoContent())
+				.andReturn();
+		
+		mvcResult = mockMVC.perform(get("/statistics"))
+				.andExpect(status().isOk())
+				.andReturn();
+		
+		
+		System.out.println(mvcResult.getResponse().getContentAsString());
 		
 
 	}
